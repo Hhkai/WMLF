311ad011ae40eae10b5af5e92cf27ba26e7c6daa
Hhkai <31685659+Hhkai@users.noreply.github.com>
2019-04-17 10:07:02 +0800

    update

--- checkcheck.py
+++ checkcheck.py
@@ -1,10 +1,13 @@
-def check():
+def check(outlog = "checklog.txt"):
+    outf = open(outlog, "w")
     with open("LF.CAL") as f:
         line = f.readline()
         line = line.strip().split(',')
     success = int(line[0])
     if success != 1:
         print success, "not success"
+        outf.write("-1\n")
+        outf.close()
         return False
     #
     with open("LF.LP1") as f:
@@ -13,9 +16,14 @@ def check():
         _ = eval(line.strip())
         if len(_) > 2:
             x = float(_[1])
-            if x < 0.95 or x > 1.05:
-                print x
-                return False
+            if x < 0.95:
+                mess = _[3], x, "L"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
+            if x > 1.05:
+                mess = _[3], x, "H"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
     ###
     dict_lp2 = dict()
     with open("LF.LP2") as f:
@@ -38,11 +46,15 @@ def check():
     for i in dict_lp2:
         if dict_l2[i][2] != 0:
             if dict_lp2[i][0] * dict_lp2[i][0] + dict_lp2[i][1] * dict_lp2[i][1] > \
-            dict_l1[dict_l2[i][0]] * dict_l1[dict_l2[i][0]] * dict_l2[i][2] * dict_l2[i][2] or \
-            dict_lp2[i][2] * dict_lp2[i][2] + dict_lp2[i][3] * dict_lp2[i][3] > \
+            dict_l1[dict_l2[i][0]] * dict_l1[dict_l2[i][0]] * dict_l2[i][2] * dict_l2[i][2] : 
+                mess = "LP2", i, "IH"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
+            if dict_lp2[i][2] * dict_lp2[i][2] + dict_lp2[i][3] * dict_lp2[i][3] > \
             dict_l1[dict_l2[i][1]] * dict_l1[dict_l2[i][1]] * dict_l2[i][2] * dict_l2[i][2] :
-                print "lp2"
-                return False
+                mess = "LP2", i, "JH"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
     #
     ##
     dict_lp3 = dict()
@@ -60,14 +72,17 @@ def check():
     for i in dict_lp3:
         if dict_l3[i] != 0:
             if dict_lp2[i][0] * dict_lp2[i][0] + dict_lp2[i][1] * dict_lp2[i][1] > \
-            dict_l3[i] * dict_l3[i] or \
-            dict_lp2[i][2] * dict_lp2[i][2] + dict_lp2[i][3] * dict_lp2[i][3] > \
             dict_l3[i] * dict_l3[i] :
-                print "LP3"
-                return False
+                mess = "LP3", i, "IH"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
+            if dict_lp2[i][2] * dict_lp2[i][2] + dict_lp2[i][3] * dict_lp2[i][3] > \
+            dict_l3[i] * dict_l3[i] :
+                mess = "LP3", i, "JH"
+                print mess
+                outf.write(str(mess)[1:-1] + "\n")
     #
     ##
-    print "check generator"
     dict_lp5 = dict()
     with open("LF.LP5") as f:
         lines = f.readlines()
@@ -81,17 +96,24 @@ def check():
         _ = eval(i.strip())
         dict_l5[_[18]] = (_[10], _[9], _[8], _[7])
     for i in dict_lp5:
-        if dict_lp5[i][0] > dict_l5[i][1] or dict_lp5[i][0] < dict_l5[i][0]:
-            if dict_l5[i][1] != 0 and dict_l5[i][0] != 0:
-                print "generator", i
-                return False
-        if dict_lp5[i][1] > dict_l5[i][3] or dict_lp5[i][1] < dict_l5[i][2]:
-            if dict_l5[i][3] != 0 and dict_l5[i][2] != 0:
-                print "generator", i
-                return False
+        if dict_lp5[i][0] > dict_l5[i][1] and dict_l5[i][1] != 0:
+            mess = "generator", i, "PH"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp5[i][0] < dict_l5[i][0] and dict_l5[i][0] != 0:
+            mess = "generator", i, "PL"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp5[i][1] > dict_l5[i][3] and dict_l5[i][3] != 0:
+            mess = "generator", i, "QH"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp5[i][1] < dict_l5[i][2] and dict_l5[i][2] != 0:
+            mess = "generator", i, "QL"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
     #
     ##
-    print "check load"
     dict_lp6 = dict()
     with open("LF.LP6") as f:
         lines = f.readlines()
@@ -105,14 +127,22 @@ def check():
         _ = eval(i.strip())
         dict_l6[_[18]] = (_[11], _[10], _[9], _[8])
     for i in dict_lp6:
-        if dict_lp6[i][0] > dict_l6[i][1] or dict_lp6[i][0] < dict_l6[i][0]:
-            if dict_l6[i][1] != 0 and dict_l6[i][0] != 0:
-                print "load", i
-                return False
-        if dict_lp6[i][1] > dict_l6[i][3] or dict_lp6[i][1] < dict_l6[i][2]:
-            if dict_l6[i][3] != 0 and dict_l6[i][2] != 0:
-                print "load", i
-                return False
+        if dict_lp6[i][0] > dict_l6[i][1] and dict_l6[i][1] != 0:
+            mess = "load", i, "PH"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp6[i][0] < dict_l6[i][0] and dict_l6[i][0] != 0:
+            mess = "load", i, "PL"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp6[i][1] > dict_l6[i][3] and dict_l6[i][3] != 0:
+            mess = "load", i, "QH"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
+        if dict_lp6[i][1] < dict_l6[i][2] and dict_l6[i][2] != 0:
+            mess = "load", i, "QL"
+            print mess
+            outf.write(str(mess)[1:-1] + "\n")
     #
     return True
     
