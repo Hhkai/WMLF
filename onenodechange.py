c6881233ec4acb63d32ea4925668b1f6065afb1e
Hhkai <31685659+Hhkai@users.noreply.github.com>
2019-05-17 23:20:17 +0800

    a

---
+++ onenodechange.py
@@ -0,0 +1,114 @@
+import time
+import MySQLdb
+import os
+
+def getflag():
+    with open("LF.CAL") as f:
+        line = f.readline()
+        line = line.strip().split(',')
+    success = int(line[0])
+    return success
+    
+def readL6():
+    mx = []
+    with open("LF.L6") as f:
+        for line in f.readlines():
+            line = list(eval(line.strip()))
+            mx.append(line)
+    return mx
+def readL5():
+    mx = []
+    with open("LF.L5") as f:
+        for line in f.readlines():
+            line = list(eval(line.strip()))
+            mx.append(line)
+    return mx
+def writeL6(mx):
+    with open("LF.L6", "w") as f:
+        for i in mx:
+            f.write(str(i)[1:-1] + '\n')
+def writeL5(mx):
+    with open("LF.L5", "w") as f:
+        for i in mx:
+            f.write(str(i)[1:-1] + '\n')
+#
+
+for L6line in range(10):
+    mx = readL6()
+    L6p = mx[L6line][4]
+    L6q = mx[L6line][5]
+    oL6p = L6p
+    oL6q = L6q
+    if L6p != 0:
+        bigp = 1.0
+        if L6p < 1:
+            bigp = 4.0
+        else:
+            bigp = 7.0
+        r = bigp / L6p
+        L6q *= r
+        L6p *= r
+    else:
+        L6p = 3.7
+        L6q = 1.893
+    mx[L6line][4] = L6p
+    mx[L6line][5] = L6q
+    writeL6(mx)
+    os.system("WMLFRTMsg")
+    success = getflag()
+    if success == 1:
+        print "success", L6line, L6p, L6q
+        mx[L6line][4] = oL6p
+        mx[L6line][5] = oL6q
+        writeL6(mx)
+        continue
+    ##### try to find the generator
+    L5mx = readL5()
+    rate = [i / 10.0 for i in range(40)]
+    beforesaved = 0
+    nosol = 1
+    for L5line in range(8):
+        if L5mx[L5line][2] == 1:
+            orip = L5mx[L5line][3]
+            oriq = L5mx[L5line][4]
+            for r in rate:
+                newp = orip * r
+                newq = oriq * r
+                L5mx[L5line][3] = newp
+                L5mx[L5line][4] = newq
+                writeL5(L5mx)
+                os.system("WMLFRTMsg")
+                success = getflag()
+                if success == 1:
+                    print "find success: ", L6line, L5line
+                    nosol = 0
+                    # store
+                    break
+            L5mx[L5line][3] = orip
+            L5mx[L5line][4] = oriq
+            writeL5(L5mx)
+        if L5mx[L5line][2] == -1:
+            orip = L5mx[L5line][3]
+            for r in rate:
+                newp = 0
+                if orip < 0:
+                    newp = orip + r
+                else:
+                    newp = orip * r
+                L5mx[L5line][3] = newp
+                writeL5(L5mx)
+                os.system("WMLFRTMsg")
+                success = getflag()
+                if success == 1:
+                    print "find success: ", L6line, L5line
+                    nosol = 0
+                    # store
+                    break
+            L5mx[L5line][3] = orip
+            writeL5(L5mx)
+    if nosol == 1:
+        print ">>>>>>>", L6line
+        a = input()
+    mx[L6line][4] = oL6p
+    mx[L6line][5] = oL6q
+    writeL6(mx)
